a15bc57fd703d1ac7eaccbdcbf87ae871fd9bf70
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-10-19 23:20:23 -0500

    Add public functions to Player class, import font, and fix indentation

--- player.h
+++ player.h
@@ -1,10 +1,11 @@
-#pragma once 
+#pragma once
 #include <SFML/Graphics.hpp>
 using namespace sf;
 
-class Player {
+class Player
+{
 
-    private:
+private:
     const float START_SPEED = 200;
     const float START_HEALTH = 100;
 
@@ -46,4 +47,55 @@ class Player {
     float m_Speed;
 
     // All our public functions
+public:
+    Player();
+    void spawn(IntRect arena, Vector2f resolution, int tileSize);
+
+    // Call this at the end of every game
+    void resetPlayerStats();
+
+    // Handle the player getting hit by a foe
+    bool hit(Time timeHit);
+
+    // How long ago was the player last hit?
+    Time getLastHitTime();
+
+    // Where is the player?
+    FloatRect getPosition();
+
+    // Where is the center of the player?
+    Vector2f getCenter();
+
+    // What angle is the player facing?
+    float getRotation();
+
+    // Send a copy of the sprite to the main function
+    Sprite getSprite();
+
+    // The next four functions move the player 
+    void moveLeft();
+    void moveRight();
+    void moveUp();
+    void moveDown();
+
+    // Stop the player moving in a specific direction
+    void stopLeft();
+    void stopRight();
+    void stopUp();
+    void stopDown();
+
+    // We will call this function once every frame
+    void update(float elapsedTime, Vector2i mousePosition);
+
+    // Give the player a speed boost
+    void upgradeSpeed();
+
+    // Give the player some health
+    void upgradeHealth();
+
+    // Increase the max health the player can have
+    void increaseHealthLevel(int amount);
+
+    // How much health does the player currently have?
+    int getHealth();
 };
