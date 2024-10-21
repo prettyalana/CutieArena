470c2cd3418131c338bd7a087347bee23f60526e
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-10-20 22:30:25 -0500

    Enable keyboard controls

--- player.cpp
+++ player.cpp
@@ -87,3 +87,43 @@ int Player::getHealth()
 {
     return m_Health;
 }
+
+void Player::moveLeft()
+{
+    m_LeftPressed = true;
+}
+
+void Player::moveRight()
+{
+    m_RightPressed = true;
+}
+
+void Player::moveUp()
+{
+    m_UpPressed = true;
+}
+
+void Player::moveDown()
+{
+    m_DownPressed = true;
+}
+
+void Player::stopLeft()
+{
+    m_LeftPressed = false;
+}
+
+void Player::stopRight()
+{
+    m_RightPressed = false;
+}
+
+void Player::stopUp()
+{
+    m_UpPressed = false;
+}
+
+void Player::stopDown()
+{
+    m_DownPressed = false;
+}
