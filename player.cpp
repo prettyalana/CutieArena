cd341398a8db23bad2175c95ba5da05b117c7429
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-10-20 22:06:17 -0500

    Spawn player function definition

--- player.cpp
+++ player.cpp
@@ -15,6 +15,26 @@ Player::Player()
     // Set the origin of the sprite to the center for smooth rotation
     m_Sprite.setOrigin(25, 25);
 
-    
-
 }
+
+// spawn function
+    void Player::spawn(IntRect arena, Vector2f resolution, int tileSize)
+    {
+        // Place the player in the middle of the arena 
+        m_Position.x = arena.width / 2;
+        m_Position.y = arena.height / 2;
+
+        // Copy the details of the arena to the player's m_Arena
+        m_Arena.left = arena.left;
+        m_Arena.width = arena.width;
+        m_Arena.top = arena.top;
+        m_Arena.height = arena.height;
+
+        // Remember how big the tiles are in this arena 
+        m_TileSize = tileSize;
+
+        // Store the resolution for future use
+        m_Resolution.x = resolution.x;
+        m_Resolution.y = resolution.y;
+
+    }
