a3b8252d7894f5e47f3b63ea9b6029e63490b10a
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-10-21 20:53:43 -0500

    Update function definition

--- player.cpp
+++ player.cpp
@@ -127,3 +127,45 @@ void Player::stopDown()
 {
     m_DownPressed = false;
 }
+
+void Player::update(float elapsedTime, Vector2i mousePosition)
+{
+    if(m_UpPressed)
+    {
+        m_Position.y -= m_Speed * elapsedTime;
+    }
+    if(m_DownPressed)
+    {
+        m_Position.y += m_Speed * elapsedTime;
+    }
+    if(m_RightPressed)
+    {
+        m_Position.x += m_Speed * elapsedTime;
+    }
+    if(m_LeftPressed) 
+    {
+        m_Position.x -= m_Speed * elapsedTime;
+    }
+
+    m_Sprite.setPosition(m_Position);
+    // Keep the player in the arena
+    if (m_Position.x > m_Arena.width - m_TileSize)
+    {
+        m_Position.x = m_Arena.width - m_TileSize;
+    }
+    if (m_Position.x < m_Arena.left + m_TileSize)
+    {
+        m_Position.x = m_Arena.left + m_TileSize;
+    }
+    if (m_Position.y > m_Arena.height - m_TileSize)
+    {
+        m_Position.y = m_Arena.height - m_TileSize;
+    }
+    if(m_Position.y < m_Arena.top + m_TileSize)
+    {
+        m_Position.y = m_Arena.top + m_TileSize;
+    }
+    // Calculate the angle the player is facing
+    float angle = (atan2 (mousePosition.y - m_Resolution.y / 2, mousePosition.x - m_Resolution.x / 2) * 180) / 3.142;
+    m_Sprite.setRotation(angle);
+}
