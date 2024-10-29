8ac68682cf87c211736d98c8fb1240ce88cb6043
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-10-28 23:23:22 -0500

    Import sprite sheet, add createBackground prototype, and define the createBackground function - Update tasks.json to use a wildcard to match all .cpp files in the root of the workspace folder (UnBearableArena)

---
+++ CreateBackground.cpp
@@ -0,0 +1,20 @@
+#include "UnBearableArena.h"
+
+int createBackground(VertexArray& rVA, IntRect arena)
+{
+    // Anything we do to rVA we are really doing to background (in the main function)
+
+    // How big is each tile/texture 
+    const int TILE_SIZE = 50;
+    const int TILE_TYPE = 3;
+    const int VERTS_IN_QUAD = 4;
+    int worldWidth = arena.width / TILE_SIZE;
+    int worldHeight = arena.height / TILE_SIZE;
+
+    // What type of primitive are we using?
+    rVA.resize(worldWidth * worldHeight * VERTS_IN_QUAD);
+
+    // Start at the beginning of the vertex array
+    int currentVertex = 0;
+    return TILE_SIZE;
+}
