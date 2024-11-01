2c289e4c4aee35fbe4feb5acff24385940379e88
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-10-31 22:09:02 -0500

    Set up the coordinates within the sprite sheet that each vertex is related to

--- CreateBackground.cpp
+++ CreateBackground.cpp
@@ -6,7 +6,7 @@ int createBackground(VertexArray& rVA, IntRect arena)
 
     // How big is each tile/texture 
     const int TILE_SIZE = 50;
-    const int TILE_TYPE = 3;
+    const int TILE_TYPES = 3;
     const int VERTS_IN_QUAD = 4;
     int worldWidth = arena.width / TILE_SIZE;
     int worldHeight = arena.height / TILE_SIZE;
@@ -31,6 +31,20 @@ int createBackground(VertexArray& rVA, IntRect arena)
             rVA[currentVertex + 2].position = Vector2f((w * TILE_SIZE) + TILE_SIZE, (h * TILE_SIZE) + TILE_SIZE);
 
             rVA[currentVertex + 3].position = Vector2f((w * TILE_SIZE), (h * TILE_SIZE) + TILE_SIZE);
+
+            // Define the position in the texture for current quad
+            // Either grass, stone bush or wall
+            if (h == 0 || h == worldHeight-1 || w == 0 || w == worldWidth-1)
+            {
+                // use the wall texture
+                rVA[currentVertex + 0].texCoords = Vector2f(0, 0 + TILE_TYPES * TILE_SIZE);
+                
+                rVA[currentVertex + 1].texCoords = Vector2f(TILE_SIZE, 0 + TILE_TYPES * TILE_SIZE);
+
+                rVA[currentVertex + 2].texCoords = Vector2f(TILE_SIZE, TILE_SIZE + TILE_TYPES * TILE_SIZE);
+
+                rVA[currentVertex + 3].texCoords = Vector2f(0, TILE_SIZE + TILE_TYPES * TILE_SIZE);
+            }
 
             // Position ready for the next four vertices 
             currentVertex = currentVertex + VERTS_IN_QUAD;
