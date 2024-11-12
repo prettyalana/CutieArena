e134757dca0b1c30a56388f221552a38ec541a57
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-11-11 23:12:53 -0600

    Create a horde of Choms

--- UnBearableArena.h
+++ UnBearableArena.h
@@ -1,4 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "Chom.h"
 using namespace sf;
-int createBackground(VertexArray& rVA, IntRect arena);
+int createBackground(VertexArray &rVA, IntRect arena);
+Chom *createHorde(int numChoms, IntRect arena);
