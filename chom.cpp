55ac3637da6b6230bf126613b6fa8abc108f229e
prettyalana <129349619+prettyalana@users.noreply.github.com>
2024-11-11 22:55:30 -0600

    Add Update function defintion

--- chom.cpp
+++ chom.cpp
@@ -78,3 +78,33 @@ Sprite Chom::getSprite()
 {
     return m_Sprite;
 }
+
+void Chom::update(float elapsedTime, Vector2f playerLocation)
+{
+    float playerX = playerLocation.x;
+    float playerY = playerLocation.y;
+
+    // Update the zombie position variables 
+    if (playerX > m_Position.x)
+    {
+        m_Position.x = m_Position.x + m_Speed * elapsedTime;
+    }
+    if (playerY > m_Position.y)
+    {
+        m_Position.y = m_Position.y + m_Speed * elapsedTime;
+    }
+
+    if (playerX < m_Position.x)
+    {
+        m_Position.x = m_Position.x - m_Speed * elapsedTime;
+    }
+    if (playerY < m_Position.y)
+    {
+        m_Position.y = m_Position.y - m_Speed * elapsedTime;
+    }
+    // Move the sprite 
+    m_Sprite.setPosition(m_Position);
+    // Face the sprite in the correct direction
+    float angle = (atan2(playerY - m_Position.y, playerX - m_Position.x) * 180) / 3.141;
+    m_Sprite.setRotation(angle);
+}
